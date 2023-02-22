0e80136ee2767976c5f400fd91033f310f9e73d6
urhStop <urh.stop@gmail.com>
2023-02-22 19:59:18 +0100

    Gravitacija.java

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,10 @@
+import java.util.*;
 public class Gravitacija{
     public static void main(String[] args) {
     System.out.println("OIS je zakon!");
+	
+	
+	System.out.println("Nadmorska višina: " + visina);
+	System.out.println("Gravitacijski pospešek: " + pospesek);
     }
 }
