58910572ab11db664ee1137ed1112945ddaa9090
pumpi11 <ll51755@student.uni-lj.si>
2023-02-22 20:18:39 +0100

    Gravitacija.java

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,16 +1,16 @@
 import java.util.*;
 public class Gravitacija{
     public static void main(String[] args) {
-    System.out.println("OIS je zakon!");
-	
+    Scanner vhod = new Scanner(System.in);
+	double visina = vhod.nextDouble();
 	
 	System.out.println("Nadmorska višina: " + visina);
-	System.out.println("Gravitacijski pospešek: " + pospesek);
+	System.out.println("Gravitacijski pospešek: " + pospesek(visina));
     }
 
-    public double pospesek(double visina) {
+    static public double pospesek(double visina) {
 
-        double pospesek = 0;
+        double pospesek = 0.0;
         double C = 6.674 * Math.pow(10, -11);
         double M = 5.972 * Math.pow(10, 24);
         double r = 6.371 * Math.pow(10, 6);
